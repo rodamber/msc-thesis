b73bc3230710707095e54c29273ae3b1bbe84efe
rodamber <rodrigoambernardo@gmail.com>
2018-10-29 21:13:18 +0000

    Removes extra stuff from Parser

--- code/src/Examples/Parser.hs
+++ code/src/Examples/Parser.hs
@@ -1,5 +1,6 @@
+{-# LANGUAGE DeriveGeneric       #-}
 {-# LANGUAGE OverloadedStrings   #-}
-{-# LANGUAGE RecordWildCards     #-}
+
 {- |
 Module:      Examples.Parser
 Description: (Will, at some point) Generate I/O examples from OutSystems expressions
@@ -14,9 +15,7 @@ Portability: ???
 module Examples.Parser where
 
 import           Control.Applicative
-import           Control.Monad                  ( forM_
-                                                , mzero
-                                                )
+import           Control.Monad                  ( mzero )
 import           Data.Aeson                     ( FromJSON(..)
                                                 , ToJSON(..)
                                                 , Value(..)
@@ -30,14 +29,8 @@ import           Data.Aeson                     ( FromJSON(..)
 import qualified Data.ByteString.Lazy.Char8    as BSL
 import           Data.Monoid
 import           Data.Text                      ( Text )
-import           System.Environment             ( getArgs )
-import           System.Exit                    ( exitFailure
-                                                , exitSuccess
-                                                )
-import           System.IO                      ( hPutStrLn
-                                                , stderr
-                                                )
 
+import GHC.Generics
 
 data Expression = Expression {
     exprText      :: Text
@@ -46,59 +39,5 @@ data Expression = Expression {
   , exprType      :: Text
   , exprFunctions :: [Text]
   , exprOmlKey    :: Text
-  } deriving (Show, Eq)
+  } deriving (Show, Eq, Generic)
 
-
-instance FromJSON Expression where
-  parseJSON (Object v) = Expression <$>
-        v .:   "text"
-    <*> v .:   "exprKey"
-    <*> v .:   "refs"
-    <*> v .:   "type"
-    <*> v .:   "functions"
-    <*> v .:   "omlKey"
-
-  parseJSON _          = mzero
-
-
-instance ToJSON Expression where
-  toJSON     Expression {..} = object [
-      "text"      .= exprText
-    , "exprKey"   .= exprExprKey
-    , "refs"      .= exprRefs
-    , "type"      .= exprType
-    , "functions" .= exprFunctions
-    , "omlKey"    .= exprOmlKey
-    ]
-
-  toEncoding Expression {..} = pairs (
-      "text"      .= exprText      <>
-      "exprKey"   .= exprExprKey   <>
-      "refs"      .= exprRefs      <>
-      "type"      .= exprType      <>
-      "functions" .= exprFunctions <>
-      "omlKey"    .= exprOmlKey
-    )
-
-
-parse :: FilePath -> IO Expression
-parse filename = do
-  input <- BSL.readFile filename
-  case decode input of
-    Nothing -> fatal $ case (decode input :: Maybe Value) of
-      Nothing -> "Invalid JSON file: " ++ filename
-      Just _  -> "Mismatched JSON value from file: " ++ filename
-    Just r -> return (r :: Expression)
- where
-  fatal :: String -> IO a
-  fatal msg = do
-    hPutStrLn stderr msg
-    exitFailure
-
-main :: IO ()
-main = do
-  filenames <- getArgs
-  forM_ filenames $ \f -> do
-    p <- parse f
-    p `seq` putStrLn $ "Successfully parsed " ++ f
-  exitSuccess
