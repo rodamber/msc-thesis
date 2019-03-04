4484ed564eff4525972a9d77c1e74cce6d527026
rodamber <rodrigoambernardo@gmail.com>
2019-03-04 16:18:08 -0500

    Update dataset test

--- code/current/src/dataset/test_dataset.py
+++ code/current/src/dataset/test_dataset.py
@@ -1,3 +1,5 @@
+import sys
+
 import jsonlines
 import parsy
 
@@ -6,9 +8,16 @@ from utils import LineError
 
 
 def test_dataset():
+    # Track how many recursion error we get
+    recursion_errors_count = 0
+
     with jsonlines.open('dataset/__exprs.jsonl') as reader:
         for line, obj in enumerate(reader, 1):
             try:
                 parse(obj['text'])
+            except RecursionError:
+                recursion_errors_count += 1
             except Exception as e:
                 raise LineError(line) from e
+
+    print(f'Got {recursion_errors_count} recursion errors', file=sys.stderr)
