da93494634511a1ea11dc44484e68739497d0199
rodamber <rodrigoambernardo@gmail.com>
2019-03-05 15:43:29 -0500

    Update process

--- code/current/src/dataset/process.py
+++ code/current/src/dataset/process.py
@@ -1,10 +1,14 @@
 import itertools
 
-from utils import LineError
+import jsonlines
+
+import expr_ast as ast
 from parser import parse
+from templatify import templatify
+from utils import LineError
 
 
-def select(infile, outfile, count, predicate):
+def select(infile, outfile, predicate, count=None):
     '''
     Selects at most count expressions from infile that satisfy predicate, and
     writes them to outfile.
@@ -24,18 +28,44 @@ def select(infile, outfile, count, predicate):
         writer.write_all(itertools.islice(json(), count))
 
 
-def main(infile, outfile, cutoff=500, count=300, size=3):
-    assert cutoff > 0 and size >= 0
-
-    def predicate(x):
-        text = x['text']
-        template = parse(text).templatify()
-        return len(text) < cutoff and template.size == size
-
-    select(infile, outfile, count, predicate)
+def templates(infile, outfile, count=None):
+    with jsonlines.open(infile) as reader:
+        with open(outfile, 'w') as f:
+            for x in itertools.islice(reader, count):
+                template = templatify(parse(x['text']))
+                f.write(f'{template}\n')
 
 
-if __name__ == '__main__':
-    # TODO import argparse
-    import sys
-    main(infile=sys.argv[1], outfile=sys.argv[2])
+# Main
+
+
+def short_pred(cutoff=300):
+    return lambda x: len(x['text']) < cutoff
+
+
+# FIXME XXX
+# Doesn't support '+' for concatenation yet
+def in_dsl(dsl):
+    def iter_(e):
+        from anytree import LevelOrderIter
+        return (node.name for node in LevelOrderIter(e.to_anytree()))
+
+    def pred(x):
+        if x['functions'] and (set(x['functions']) <= set(dsl)):
+            template = templatify(parse(x['text']))
+
+            # Filter unwanted operations
+
+            no_binops = not any(
+                isinstance(x, ast.Binop) for x in iter_(template))
+            if no_binops:
+                no_unops = not any(
+                    isinstance(x, ast.Unop) for x in iter_(template))
+                return no_unops
+        return False
+
+    return pred
+
+
+def ast_size(size):
+    return lambda x: templatify(parse(x['text'])).size() == size
--- code/current/src/dataset/script.py
+++ code/current/src/dataset/script.py
@@ -1,14 +1,53 @@
+import itertools
 import jsonlines
 import sys
-
-from expr_ast import *
-from parser import *
 
 dataset = '../../dataset/__exprs.jsonl'
 
 
-def read_line(n):
+def read_line(n, dataset=dataset, field='text'):
     with jsonlines.open(dataset) as reader:
         for line, obj in enumerate(reader, 1):
             if line == n:
-                return obj['text']
+                return obj[field]
+
+
+def simplify(src=dataset, dst='simplify'):
+    with jsonlines.open(src, 'r') as reader:
+        with jsonlines.open(dst, 'w') as writer:
+            for x in reader:
+                writer.write({'text': x['text'], 'functions': x['functions']})
+
+
+# Demonstration
+
+import expr_ast as ast
+from parser import parse
+from templatify import templatify
+
+expr = parse(read_line(15))
+template = templatify(expr)
+
+# components = to_components(template)
+
+# inputs = {component: gen_inputs(component, 10) for component in components}
+# outputs = {
+#     component: ((input_, component(input_)) for input_ in inputs[component])
+#     for component in components
+# }
+
+# Hypothesis demonstration
+
+from hypothesis import assume, strategies as st
+from string import ascii_letters
+
+
+@st.composite
+def substr(draw, alphabet=ascii_letters):
+    text = draw(st.text(alphabet, min_size=1))
+
+    middle = draw(st.integers(0, len(text) - 1))
+    left = draw(st.integers(0, middle))
+    right = draw(st.integers(middle, len(text) - 1))
+
+    return (text, left, right)
