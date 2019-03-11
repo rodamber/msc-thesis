ddbfc676477b0a4218234d057ed0c16e1db8b32e
rodamber <rodrigoambernardo@gmail.com>
2019-03-11 18:44:04 -0400

    Add __init__ and a main to the source root dir

--- code/current/src/main.py
+++ code/current/src/main.py
@@ -1,4 +1,8 @@
-from synthesis.dataset.process import templatify
+from synthesis.outsystems.parser import parse
+from synthesis.outsystems.type_infer import type_infer_leaves
 
 if __name__ == '__main__':
-    templatify()
+    pass
+
+tree = parse('substr("outsystems.com", 0, index("outsystems.com", "."))')
+type_infer_leaves(tree)
