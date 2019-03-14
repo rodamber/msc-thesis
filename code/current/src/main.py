f807734b301b8c6956aaba290d49fa7e4314fda7
rodamber <rodrigoambernardo@gmail.com>
2019-03-14 04:06:08 -0400

    Add buggy constraint gen of program line encoding with enum

--- code/current/src/main.py
+++ code/current/src/main.py
@@ -1,4 +1,11 @@
-from synthesis.smt.synthesizer import *
+from synthesis.smt.enum_lines import *
 
-if __name__ == '__main__':
-    test_synth()
+e1 = Example(inputs=p.v('John', 'Doe'), output='John Doe')
+e2 = Example(inputs=p.v('Jane', 'Done'), output='Jane Done')
+examples = p.v(e1, e2)
+
+program = generate_program(p.v(concat, concat), examples)
+pretty_program(program, e1)
+
+# library = p.v(concat)
+print(synth(library, examples, program_size=2))
--- code/current/src/synthesis/smt/enum_lines.py
+++ code/current/src/synthesis/smt/enum_lines.py
@@ -1,86 +1,403 @@
+from collections import Counter, namedtuple
+from contextlib import suppress
+from dataclasses import dataclass
+from itertools import (chain, combinations, combinations_with_replacement,
+                       product)
+
 import pyrsistent as p
 import z3
 
-'''
------------
-Definitions
------------
-
-H := set of all holes
-I := set of all program inputs
-O := set of all component outputs
-X := H or I or O
-C := set of all components
-E := set of all examples
-M := set of magic constants
-
-S_e := S and e
-l_x := line number where x is defined
-
-holes(c)     := holes of component c
-line(c)      := line (python int) of component c
-output(c, e) := output (z3 constant) of component c on example e
-last(cs)     := component that appears last in the program (highest line number)
-
-component(h) := component of which hole h is part of
-
-holes(e)   := holes of all components in example e
-inputs(e)  := inputs of example e
-outputs(e) := outputs of all components in example e
-
------------
-Constraints
------------
-
-We need to make sure that:
-
-- output soundness
-- aciclicity
-- all program inputs are used on each example
-- all component outputs are used on each example
-- for every example, the output of the last component equals the output of the
-  example
-
-01. 1 <= l_m <= |M|                          for m in M
-02. |M| + 1 <= l_i <= I                      for i in I
-03. l_{output(c, e)} == line(c)              for c in C, e in E
-04. 1 <= l_h <= line(c) + 1                  for c in C, h in holes(c)
-05. l_x != l_y                               for x in X, y in X
-                                               if sort(x) != sort(y)
-06. l_x == l_y => x == y                     for e in E, x in holes(e),
-                                               y in (inputs(e) or outputs(e))
-                                               if sort(x) == sort(y)
-07. (l_x == l_y and l_x <= |M|) => x == y    for x in H, y in H
-                                               if sort(x) == sort(y)
-08. output(c) == c(x1, x2, ..., xn)          for c in C,
-                                               x1, x2, ..., xn in holes(c)
-09. Or_{h in holes(e)}(l_h == l_i)           for i in I, e in E
-                                               if sort(h) == sort(i)
-10. Or_{h in {h in holes(e) | line(component(h) > line(component(o)))}(l_h == l_o)}
-                                             for o in O, e in E
-                                               if sort(h) == sort(o)
-11. output(last(C), e) == output(e)          for e in E
-
-In English:
-
-01. Define the range for the lines of the magic constants
-02. Define the range for the lines of the input constants
-03. Define the range for the lines of the output constants on each example
-04. Define the range for the lines of the hole constants
-05. Two constants of the same sort cannot share their line.
-06. In a given example, assert that if a hole is defined on the same line as an
-input, or an output of some component, then its value must be equal to the value
-of that input, or output, respectively. Do this for every example.
-07. Two holes that share a line where a magic constant is defined must be equal
-across all examples.
-08. On a given example, the output of each component must be the result of
-running that component with the values of its holes as inputs.
-09. Every program input must be used on every example.
-10. Every component output must be used on every example.
-11. The output of the last component must equal the output of the example.
+# -----
+# Types
+# -----
 
 
-Constraint generation should be aborted if no constraints need to be generated
-on points 09 and 10.
+class Component(p.PClass):
+    name = p.field(type=str)
+    domain = p.pvector_field(item_type=type)
+    ret_type = p.field(mandatory=True, type=type)
+    function = p.field(mandatory=True, type=object)
 
-'''
+
+class Example(p.PClass):
+    inputs = p.pvector_field(object)
+    output = p.field(mandatory=True, type=object)
+
+
+class Lineno(p.PClass):
+    get = p.field(mandatory=True, type=object)
+
+
+class Const(p.PClass):
+    get = p.field(mandatory=True, type=object)
+    lineno = p.field(mandatory=True, type=Lineno)
+
+
+class Input(p.PClass):
+    map = p.pmap_field(key_type=Example, value_type=object)
+    lineno = p.field(mandatory=True, type=Lineno)
+
+
+class Output(p.PClass):
+    map = p.pmap_field(key_type=Example, value_type=object)
+    lineno = p.field(mandatory=True, type=Lineno)
+
+
+class Hole(p.PClass):
+    map = p.pmap_field(key_type=Example, value_type=object)
+    lineno = p.field(mandatory=True, type=Lineno)
+
+
+class ProgramLine(p.PClass):
+    output = p.field(mandatory=True, type=Output)
+    component = p.field(mandatory=True, type=Component)
+    holes = p.pvector_field(item_type=Hole)
+
+
+class Program(p.PClass):
+    # consts = p.pvector_field(item_type=Const)
+    inputs = p.pvector_field(item_type=Input)
+    lines = p.pvector_field(item_type=ProgramLine)
+
+
+class UnplugableComponents(Exception):
+    pass
+
+
+class UnusableInput(Exception):
+    pass
+
+
+# ---------------
+# Pretty printing
+# ---------------
+
+
+# TODO This could be better.
+# FIXME It orders lines as they appear in the record, not by the value assigned
+# to z3 (duh)
+def pretty_program(prog, example):
+    for pi, pj in zip(prog.inputs, example.inputs):
+        pretty_input(pi, pj, example)
+    for l in prog.lines:
+        pretty_line(l, example)
+
+
+def pretty_input(pi, pj, example):
+    const = pi.map[example]
+    print(f'{pi.lineno.get} | {const} = {repr(pj)}')
+
+
+def pretty_line(l, example):
+    pretty_output(l.output, example)
+    pretty_component(l.component)
+    pretty_holes(l.holes, example)
+
+
+def pretty_output(o, example):
+    const = o.map[example]
+    print(f'{o.lineno.get} | {const}', end=' = ')
+
+
+def pretty_component(c):
+    print(c.name, end='')
+
+
+def pretty_holes(hs, example):
+    consts = map(lambda h: str(h.map[example]), hs)
+    print('(' + ', '.join(consts) + ')')
+
+
+# ----------
+# Components
+# ----------
+
+concat = Component(
+    name='concat',
+    domain=(str, str),
+    ret_type=str,
+    function=lambda x, y: z3.Concat(x, y))
+index = Component(
+    name='index',
+    domain=(str, str),
+    ret_type=int,
+    function=lambda text, x: z3.IndexOf(text, x, 0))
+length = Component(
+    name='length',
+    domain=(str, ),
+    ret_type=int,
+    function=lambda x: z3.Length(x))
+replace = Component(
+    name='replace',
+    domain=(str, str, str),
+    ret_type=str,
+    function=lambda x, y, z: z3.Replace(x, y, z))
+substr = Component(
+    name='substr',
+    domain=(str, int, int),
+    ret_type=str,
+    function=lambda text, i, j: z3.SubString(text, i, j - i))
+
+library = p.v(concat, index, length, replace, substr)
+
+# -----
+# Utils
+# -----
+
+
+def type2sort(typ):
+    if typ == int:
+        return z3.IntSort()
+    elif typ == str:
+        return z3.StringSort()
+    else:
+        raise ValueError(f'Unsupported type: {typ}')
+
+
+def z3_fresh_const(typ, prefix='c'):
+    return z3.FreshConst(type2sort(typ), prefix)
+
+
+def z3_fresh_line():
+    return z3_fresh_const(int, prefix='l')
+
+
+def z3_val(val):
+    if isinstance(val, int):
+        return z3.IntVal(val)
+    elif isinstance(val, str):
+        return z3.StringVal(val)
+    else:
+        raise ValueError(f'z3_val: invalid typ ({typ})')
+
+
+# ---------
+# Constants
+# ---------
+
+
+def generate_program(components, examples):
+    # consts = generate_consts(components, examples)
+    inputs = generate_inputs(examples)
+    lines = generate_program_lines(components, examples)
+
+    # return Program(consts=consts, inputs=inputs, lines=lines)
+    return Program(inputs=inputs, lines=lines)
+
+
+# TODO While this doesn't seem to be strictly necessary, it is nice to have
+# for pretty printing.
+# def generate_consts(components, examples):
+#     type_counter = Counter()
+
+#     import pdb; pdb.set_trace()
+
+#     for c in components:
+#         for typ in c.domain:
+#             type_counter[typ] += 1
+#         type_counter[c.ret_type] -= 1
+
+#     for i in examples[0].inputs:
+#         type_counter[type(i)] -= 1
+
+#     return p.pvector(
+#         Const(get=z3_fresh_const(typ, prefix='c'),
+#               lineno=Lineno(get=z3_fresh_line()))
+#         for typ, cnt in type_counter.items()
+#         for _ in range(cnt + 1))
+
+
+def generate_inputs(examples):
+    return p.pvector(
+        Input(
+            map=p.pmap((e, z3_fresh_const(type(i), prefix='i'))
+                       for e in examples),
+            lineno=Lineno(get=z3_fresh_line())) for i in examples[0].inputs)
+
+
+def generate_program_lines(components, examples):
+    outputs = generate_outputs(components, examples)
+    holes = p.pvector(generate_holes(c, examples) for c in components)
+    lines = p.pvector(
+        ProgramLine(output=o, component=c, holes=hs)
+        for o, c, hs in zip(outputs, components, holes))
+
+    return lines
+
+
+def generate_outputs(components, examples):
+    return p.pvector(
+        Output(
+            map=p.pmap((e, z3_fresh_const(c.ret_type, prefix='o'))
+                       for e in examples),
+            lineno=Lineno(get=z3_fresh_line())) for c in components)
+
+
+def generate_holes(component, examples):
+    return p.pvector(
+        Hole(
+            map=p.pmap((e, z3_fresh_const(typ, prefix='h')) for e in examples),
+            lineno=Lineno(get=z3_fresh_line())) for typ in component.domain)
+
+
+# -----------
+# Constraints
+# -----------
+
+
+def generate_constraints(program, examples):
+    inputs = program.inputs
+    outputs = p.pvector(l.output for l in program.lines)
+    holes = p.pvector(h for l in program.lines for h in l.holes)
+
+    input_count = len(examples[0].inputs)
+    component_count = len(program.lines)
+    hole_count = sum(len(l.component.domain) for l in program.lines)
+
+    yield from gen_input_line_constraints(inputs, hole_count, component_count)
+    yield from gen_output_line_constraints(outputs, hole_count,
+                                           component_count)
+    yield from gen_hole_line_constraints(program, examples)
+    yield from gen_sort_line_constraints(inputs, holes, outputs)
+    yield from gen_well_formedness_constraints(holes, inputs, outputs,
+                                               examples)
+    yield from gen_constant_constraints(holes, component_count, input_count)
+    yield from gen_output_soundness_constraints(program.lines, examples)
+    yield from gen_input_output_completeness_constraints(
+        inputs, outputs, holes, examples)
+    yield from gen_correctness_constraints(hole_count, program.lines, examples)
+    yield from gen_input_value_constraints(inputs, examples)
+
+
+def gen_input_line_constraints(inputs, hole_count, component_count):
+    start = hole_count - component_count - len(inputs) + 1
+
+    for lineno, i in enumerate(inputs, start):
+        yield i.lineno.get == lineno
+
+
+def gen_output_line_constraints(outputs, hole_count, component_count):
+    start = hole_count + 1 - component_count
+
+    for lineno, o in enumerate(outputs, start):
+        yield o.lineno.get == lineno
+
+
+def gen_hole_line_constraints(program, examples):
+    for line in program.lines:
+        for hole in line.holes:
+            yield z3.And(
+                z3_val(1) <= hole.lineno.get,
+                hole.lineno.get <= line.output.lineno.get)
+
+
+def gen_sort_line_constraints(inputs, outputs, holes):
+    all_consts = p.pvector(
+        (l, x) for i, o, h in zip(inputs, outputs, holes)
+        for l, x in zip((i.lineno, o.lineno,
+                         h.lineno), (*i.map.values(), *o.map.values(),
+                                     *h.map.values())))
+
+    for (l1, c1), (l2, c2) in combinations(all_consts, r=2):
+        if c1.sort() != c2.sort():
+            yield l1 != l2
+
+
+def gen_well_formedness_constraints(holes, inputs, outputs, examples):
+    for e in examples:
+        for h, c in product(holes, p.v(*inputs, *outputs)):
+            h_const = h.map[e]
+            c_const = c.map[e]
+
+            if h_const.sort() == c_const.sort():
+                yield z3.Implies(h.lineno.get == c.lineno.get,
+                                 h_const == c_const)
+
+
+def gen_constant_constraints(holes, component_count, input_count):
+    hole_count = len(holes)
+    consts_count = hole_count - component_count - input_count
+
+    all_hole_consts = p.pvector(
+        (c, h.lineno.get) for h in holes for c in h.map.values())
+
+    for (c1, l1), (c2, l2) in combinations(all_hole_consts, r=2):
+        if c1.sort() != c2.sort() and c1 is not c2:
+            yield z3.Implies(
+                z3.And(l1 == l2, l1 <= z3_val(consts_count)), c1 == c2)
+
+
+def gen_output_soundness_constraints(program_lines, examples):
+    for line in program_lines:
+        for e in examples:
+            output = line.output.map[e]
+            component = line.component.function
+            holes = p.pvector(h.map[e] for h in line.holes)
+
+            yield output == component(*holes)
+
+
+def gen_input_output_completeness_constraints(inputs, outputs, holes,
+                                              examples):
+    for i, o in zip(inputs, outputs):
+        for e in examples:
+            input_constraints = p.pvector(
+                i.map[e] == h.map[e] for h in holes
+                if i.map[e].sort() == h.map[e].sort())
+
+            if not input_constraints:
+                raise UnusableInput()
+
+            output_constraints = p.pvector(
+                o.map[e] == h.map[e] for h in holes
+                if o.map[e].sort() == h.map[e].sort())
+
+            if not output_constraints:
+                raise UnplugableComponents()
+
+            yield z3.And(z3.Or(*input_constraints), z3.Or(*output_constraints))
+
+
+def gen_correctness_constraints(hole_count, program_lines, examples):
+    last_lineno = z3_val(hole_count + 1)
+    for e in examples:
+        for l in program_lines:
+            if l.output.map[e].sort() != type2sort(type(examples[0].output)):
+                yield l.output.lineno.get != last_lineno
+            else:
+                yield z3.Implies(l.output.lineno.get == last_lineno,
+                                 l.output.map[e] == z3_val(e.output))
+
+
+def gen_input_value_constraints(inputs, examples):
+    for e in examples:
+        for i, ei in zip(inputs, e.inputs):
+            yield i.map[e] == z3_val(ei)
+
+
+# ---------
+# Synthesis
+# ---------
+
+# def reconstruct(program, model):
+#     for i in program.inputs:
+#         i.
+
+
+def synth(library, examples, program_size):
+    for components in combinations_with_replacement(library, program_size):
+        import pdb; pdb.set_trace()
+        solver = z3.Solver()
+
+        with suppress(UnplugableComponents, UnusableInput):
+            program = generate_program(components, examples)
+            solver.add(*generate_constraints(program, examples))
+
+            print(list(map(lambda c: c.name, components)), solver.check())
+            if solver.check() == z3.sat:
+                model = solver.model()
+
+                print(f'SAT! Model:\n{model}')
+                return solver
+    else:
+        print('Unable to synthesize :\'(')
