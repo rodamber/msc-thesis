bb3dc427a9bca5437c43bfffb9ce6c25c2639215
rodamber <rodrigoambernardo@gmail.com>
2019-03-18 20:32:10 -0400

    Add oneliner pretty print utility

--- code/current/src/main.py
+++ code/current/src/main.py
@@ -8,24 +8,31 @@ from synthesis.smt.lines.pretty import *
 from synthesis.smt.lines.synth import *
 
 test_set = p.v(
+    # p.v(
+    #     Example(inputs=p.v('John', 'Doe'), output='John Doe'),
+    #     Example(inputs=p.v('Anne', 'Smith'), output='Anne Smith')),
+    # p.v(Example(inputs=p.v('outsystems', 'com'), output='outsystems.com')),
+    # p.v(
+    #     Example(inputs=p.v('outsystems.com'), output='outsystems'),
+    #     Example(inputs=p.v('cmu.edu'), output='cmu')),
+    # p.v(
+    #     Example(inputs=p.v('outsystems.com'), output='outsystems'),
+    #     Example(inputs=p.v('cmu.edu'), output='cmu'),
+    #     Example(inputs=p.v('tecnico.pt'), output='tecnico')),
+    # p.v(
+    #     Example(inputs=p.v('outsystems.com'), output='outsystems@com'),
+    #     Example(inputs=p.v('cmu.edu'), output='cmu@edu')),
     p.v(
-        Example(inputs=p.v('John', 'Doe'), output='John Doe'),
-        Example(inputs=p.v('Anne', 'Smith'), output='Anne Smith')),
-    p.v(Example(inputs=p.v('outsystems', 'com'), output='outsystems.com')),
-    p.v(
-        Example(inputs=p.v('outsystems.com'), output='outsystems'),
-        Example(inputs=p.v('cmu.edu'), output='cmu')),
-    p.v(
-        Example(inputs=p.v('outsystems.com'), output='outsystems'),
-        Example(inputs=p.v('cmu.edu'), output='cmu'),
-        Example(inputs=p.v('tecnico.pt'), output='tecnico')),
-    p.v(
-        Example(
-            inputs=p.v('rodrigo', 'bernardo', 'gmail', 'com'),
-            output='rodrigo.bernardo@gmail.com'),
-        Example(
-            inputs=p.v('pedro', 'orvalho', 'tecnico', 'pt'),
-            output='pedro.orvalho@tecnico.pt')),
+        Example(inputs=p.v('outsystems', 'com'), output='outsystems.com'),
+        Example(inputs=p.v('cmu', 'edu'), output='cmu.edu')),
+
+    # p.v(
+    #     Example(
+    #         inputs=p.v('rodrigo', 'bernardo', 'gmail', 'com'),
+    #         output='rodrigo.bernardo@gmail.com'),
+    #     Example(
+    #         inputs=p.v('pedro', 'orvalho', 'tecnico', 'pt'),
+    #         output='pedro.orvalho@tecnico.pt')),
 )
 
 for test_case in test_set:
@@ -41,4 +48,5 @@ for test_case in test_set:
         program, model = res
         example = test_case[0]
 
-        pretty_program(program, model, example=test_case[0])
+        pretty_lines(program, model, example=test_case[0])
+        pretty_oneliner(program, model)
--- code/current/src/synthesis/smt/lines/pretty.py
+++ code/current/src/synthesis/smt/lines/pretty.py
@@ -6,36 +6,61 @@ from .types import Const
 from .utils import z3_as
 
 
+def eval(c, model):
+    # XXX
+    with suppress(ValueError):
+        return z3_as(model[c])
+
+
+def pretty_oneliner(program, model):
+    import string
+
+    show = {}
+    inputs = dict(
+        (i.lineno, x) for i, x in zip(program.inputs, string.ascii_lowercase))
+
+    for c in program.consts:
+        show.update({eval(c.lineno.get, model): repr(eval(c.get, model))})
+
+    for i in program.inputs:
+        show.update({eval(i.lineno.get, model): inputs[i.lineno]})
+
+    for line in sorted(
+            program.lines, key=lambda x: eval(x.output.lineno.get, model)):
+        hs = (show[eval(h.lineno.get, model)] for h in line.holes)
+        s = line.component.name + '(' + ', '.join(hs) + ')'
+        show.update({eval(line.output.lineno.get, model): s})
+
+    print(f"({', '.join(inputs.values())}) \u21A6 ", end='')
+    print(show[max(show, key=show.get)])
+
+
 # XXX
-def pretty_program(program, model, example=None):
+def pretty_lines(program, model, example=None):
     consts = program.consts
     inputs = program.inputs
     outputs = p.pvector(line.output for line in program.lines)
     holes = p.pvector(h for line in program.lines for h in line.holes)
 
-    def eval(c):
-        # XXX
-        with suppress(ValueError):
-            return z3_as(model[c])
-
-    line2val = p.pmap(
-        (eval(c.lineno.get), c) for c in p.v(*consts, *inputs, *outputs))
+    line2val = p.pmap((eval(c.lineno.get, model), c)
+                      for c in p.v(*consts, *inputs, *outputs))
 
     for c in program.consts:
-        x = f'{eval(c.lineno.get)} | c_{eval(c.lineno.get)} = {repr(eval(c.get))}'
+        x = f'{eval(c.lineno.get, model)} | c_{eval(c.lineno.get, model)} = {repr(eval(c.get, model))}'
         print(x)
 
     for n, i in enumerate(program.inputs, 1):
-        print(f'{eval(i.lineno.get)} | {i.map.values()[0]} = _{n}')
+        print(f'{eval(i.lineno.get, model)} | {i.map.values()[0]} = _{n}')
 
-    for l in sorted(program.lines, key=lambda l: eval(l.output.lineno.get)):
+    for l in sorted(
+            program.lines, key=lambda l: eval(l.output.lineno.get, model)):
         print(
-            f'{eval(l.output.lineno.get)} | {l.output.map.values()[0]}',
+            f'{eval(l.output.lineno.get, model)} | {l.output.map.values()[0]}',
             end=' = ')
         print(l.component.name, end='')
 
         def hole_ref(h):
-            c = line2val[eval(h.lineno.get)]
+            c = line2val[eval(h.lineno.get, model)]
             if isinstance(c, Const):
                 return c.get
             else:
@@ -46,4 +71,4 @@ def pretty_program(program, model, example=None):
             end=' ' if example else '\n')
 
         if example:
-            print(f'# {repr(eval(l.output.map[example]))}')
+            print(f'# {repr(eval(l.output.map[example], model))}')
