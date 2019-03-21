f5f6cf43e0fe0285bd73cee12a2bacb84bfca583
rodamber <rodrigoambernardo@gmail.com>
2019-03-20 21:20:48 -0400

    parametrize tests

--- code/current/test/test_all.py
+++ code/current/test/test_all.py
@@ -1,4 +1,7 @@
+import pytest
+
 from src.synthesis.smt.lines.synth import synth
+
 from .cases import (concat, index, length, replace, substr, substr_concat,
                     substr_index)
 from .timewith import timewith
@@ -6,15 +9,14 @@ from .timewith import timewith
 all_cases = concat.cases + index.cases + length.cases + replace.cases + \
     substr.cases + substr_concat.cases + substr_index.cases
 
+@pytest.mark.parametrize('examples', all_cases)
+def test_synth(examples):
+    with timewith('synthesis test') as timer:
+        is_ok, _ = synth(examples, timeout=1000)
+        elapsed = timer.elapsed
 
-def test_all():
-    for case in all_cases:
-        with timewith('synthesis test') as timer:
-            is_ok, _ = synth(case)
-            elapse = timer.elapsed
-
-        assert is_ok
-        assert elapsed <= 60  # one minute
+    assert is_ok
+    assert elapsed <= 60  # one minute
 
 
 if __name__ == '__main__':
