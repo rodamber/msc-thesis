30f43ff404c1b9389421d7d9fa15e63bc9023814
rodamber <rodrigoambernardo@gmail.com>
2019-03-23 02:14:41 -0400

    Add comment

--- code/current/src/test_cases/substr_index.py
+++ code/current/src/test_cases/substr_index.py
@@ -1,20 +1,25 @@
 from synthesis.types import example
 
 cases = (
+    # 00
     [example(('outsystems.com', ), 'outsystems')],
+    # 01
     [
         example(('outsystems.com', ), 'outsystems'),
         example(('cmu.edu', ), 'cmu')
     ],
+    # 02
     [
         example(('outsystems.com', ), 'outsystems'),
         example(('cmu.edu', ), 'cmu'),
         example(('tecnico.pt', ), 'tecnico')
     ],
+    # 03
     [
         example(('outsystems.com', '.'), 'outsystems'),
         example(('cmu.edu', '.'), 'cmu')
     ],
+    # 04
     [
         example(('www.outsystems.com', ), 'outsystems'),
         example(('www.cmu.edu', ), 'cmu'),
