25bc795ca1ec58a37cf3cb99da8ab7752d332d81
rodamber <rodrigoambernardo@gmail.com>
2019-03-26 01:18:30 -0400

    Add more test cases to substr_index.py

--- code/current/src/test_cases/substr_index.py
+++ code/current/src/test_cases/substr_index.py
@@ -25,4 +25,39 @@ cases = (
         example(('www.cmu.edu', ), 'cmu'),
         example(('www.tecnico.pt', ), 'tecnico')
     ],
+    # 05
+    [
+        example(('www.outsystems.com', ), 'outsystems'),
+        example(('www.cmu.edu', ), 'cmu'),
+        example(('www3.tecnico.pt', ), 'tecnico')
+    ],
+    # 06
+    [
+        example(('www.outsystems.com', ), 'outsystems'),
+        example(('www.cmu.edu', ), 'cmu'),
+        example(('www3.tecnico.pt', ), 'tecnico'),
+        example(['https://www.tecnico.pt'], 'tecnico'),
+    ],
 )
+
+# INFO:Started synthesis tests
+# INFO:Examples:
+# INFO:   ('www.outsystems.com',) --> 'outsystems'
+# INFO:   ('www.cmu.edu',) --> 'cmu'
+# INFO:   ('www3.tecnico.pt',) --> 'tecnico'
+# INFO:   ('https://www.tecnico.pt',) --> 'tecnico'
+# DEBUG:Library of components:
+# DEBUG:  index
+# DEBUG:  index3
+# DEBUG:  substr
+# DEBUG:  minus
+# DEBUG:  add
+# DEBUG:Min size: 5
+# DEBUG:Max size: 5
+# DEBUG:Z3 max_conflicts: 100000
+# DEBUG:Solver result: SAT (177.490 seconds)
+# INFO:Elapsed time: 177.873 seconds
+# INFO:Program: (a) ↦ substr(a, add(1, index(a, '.')), minus(index3(a, '.',
+# add(1, inde x(a, '.'))), add(1, index(a, '.'))))
+# INFO:==================================================
+# INFO:Finished synthesis tests
