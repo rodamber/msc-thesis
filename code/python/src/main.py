709781c764b8a8f6c780ee162c95ad0980f4c0bd
rodamber <rodrigoambernardo@gmail.com>
2019-04-24 14:59:54 -0400

    Add small cmd line utility
    done to able to record a usage example for outsystems

--- code/python/src/main.py
+++ code/python/src/main.py
@@ -1,3 +1,4 @@
+import fileinput
 import time
 
 import z3
@@ -7,7 +8,7 @@ from simple import (IO, Kind, add, concat, index, length, newline, replace,
                     sub, substr, synthesize, to_lower, to_upper, trim,
                     trim_end, trim_start)
 
-stdlib = (concat, index, length, newline, replace, substr, to_lower, to_upper,
+stdlib = (concat, index, length, replace, substr, to_lower, to_upper,
           trim, trim_start, trim_end, add, sub)
 
 stack = [(Kind.INT, 2), (Kind.STR, 2)]
@@ -29,13 +30,41 @@ def bench(ios):
     print('==================================================')
 
 
-def main():
+def bench_all():
     print('Started synthesis tests')
 
     for ios in test_cases.all_test_cases():
         bench(ios)
 
     print('Finished synthesis tests')
+
+
+def examples_stdin():
+    for line in fileinput.input():
+        if line == 'synth\n':
+            return
+
+        contents = line.rstrip().split(',')
+        inputs = tuple(contents[:-1])
+        output = contents[-1]
+
+        yield IO(inputs, output)
+
+
+def main():
+    lib = (concat, index, length, # replace,
+           substr,
+          # to_lower, to_upper,
+          # trim, trim_start, trim_end, add, sub
+    )
+    ios = list(examples_stdin())
+    stack = [(Kind.INT, 2), (Kind.STR, 2)]
+    prog = synthesize(ios=ios, lib=lib, stack=stack)
+
+    if prog:
+        print(f'Program: {prog}')
+    else:
+        print(f'No program found.')
 
 
 if __name__ == '__main__':
