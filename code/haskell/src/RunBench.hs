499d004cbd904ef4983062f44fa52fa913903e35
rodamber <rodrigoambernardo@gmail.com>
2019-04-28 00:55:41 -0400

    Add runbench and runsynth

---
+++ code/haskell/src/RunBench.hs
@@ -0,0 +1,96 @@
+module RunBench where
+
+import           Control.Monad
+import           System.Directory
+import           System.Process
+
+import           Options.Applicative
+
+import qualified RunSynth            as S hiding (main)
+
+
+data Opts = Opts
+  Int -- ^ wall-clock time limit in seconds
+  Int -- ^ memory limit in gigabytes
+  FilePath -- ^ output directory
+  [S.OptFile] -- ^ files with examples
+  S.OptNumExamples -- ^ number of examples to take from each benchmark
+  S.OptNumConsts -- ^ number of constants to use
+
+
+timeLimit (Opts t _ _ _ _ _) = t
+memLimit (Opts _ m _ _ _ _) = m
+outputDir (Opts _ _ d _ _ _) = d
+files (Opts _ _ _ fs _ _) = fs
+numExamples (Opts _ _ _ _ (S.OptNumExamples e) _) = e
+numConsts (Opts _ _ _ _ _ (S.OptNumConsts c)) = c
+
+
+opts :: Parser Opts
+opts = Opts <$>
+  optTimeLimit <*>
+  optMemLimit <*>
+  optOutputDir <*>
+  some S.optFile <*>
+  S.optNumExamples <*>
+  S.optNumConsts
+
+
+optTimeLimit :: Parser Int
+optTimeLimit = option auto (long "time-limit" <>
+                            short 't' <>
+                            metavar "TIME-LIMIT" <>
+                            showDefault <>
+                            value 1 <>
+                            help "Time limit (wall-clock time) in seconds")
+
+
+optMemLimit :: Parser Int
+optMemLimit = option auto (long "mem-limit" <>
+                           short 'm' <>
+                           metavar "MEM-LIMIT" <>
+                           showDefault <>
+                           value 12 <>
+                           help "Memory limit in gigabytes")
+
+
+optOutputDir :: Parser FilePath
+optOutputDir = strOption (long "output-dir" <>
+                          short 'd' <>
+                          metavar "OUTPUT-DIR" <>
+                          help "Directory where to save the benchmarks")
+
+
+optsInfo :: ParserInfo Opts
+optsInfo = info (opts <**> helper)
+           (fullDesc <> header "outsynth2 benchmark runner")
+
+
+ensureRunsolver :: IO ()
+ensureRunsolver = void $ readProcess "which" ["runsolver"] ""
+
+
+runBench :: Opts -> IO ()
+runBench opts = do
+  ensureRunsolver
+
+  createDirectoryIfMissing True (outputDir opts)
+
+  forM_ (files opts) $ \(S.OptFile file) ->
+    readProcess "runsolver" [ "--wall-clock-limit"
+                            , show $ timeLimit opts
+                            , "--mem-limit"
+                            , show $ memLimit opts * 1000 -- megabytes
+                            , "--solver-data"
+                            , outputDir opts <> "/" <> file <> ".out"
+                            , "outsynth2"
+                            , "--examples"
+                            , show $ numExamples opts
+                            , "--constants"
+                            , show $ numConsts opts
+                            , file
+                            ] ""
+
+
+main :: IO ()
+main = runBench =<< execParser optsInfo
---
+++ code/haskell/src/RunSynth.hs
@@ -0,0 +1,104 @@
+module RunSynth
+  (OptNumExamples(..),
+   OptNumConsts(..),
+   OptFile(..),
+   Opts(..),
+   optNumExamples,
+   optNumConsts,
+   optFile,
+   runSynth,
+   main) where
+
+import           Options.Applicative
+import           System.TimeIt
+
+import           Components
+import           ReadExample
+import           Synth
+import           Types
+
+
+newtype OptNumExamples = OptNumExamples Int
+newtype OptNumConsts = OptNumConsts Int
+newtype OptFile = OptFile FilePath
+
+data Opts = Opts OptNumExamples OptNumConsts OptFile
+
+
+numExamples :: Opts -> Int
+numExamples (Opts (OptNumExamples e) _ _) = e
+
+
+numConsts :: Opts -> Int
+numConsts (Opts _ (OptNumConsts c) _) = c
+
+
+file :: Opts -> FilePath
+file (Opts _ _ (OptFile f)) = f
+
+
+opts :: Parser Opts
+opts = Opts <$> optNumExamples <*> optNumConsts <*> optFile
+
+
+optNumExamples :: Parser OptNumExamples
+optNumExamples = OptNumExamples <$>
+  option auto (long "examples" <>
+               short 'e' <>
+               metavar "EXAMPLES" <>
+               showDefault <>
+               value 1 <>
+               help "How many examples to use")
+
+
+optNumConsts :: Parser OptNumConsts
+optNumConsts = OptNumConsts <$>
+  option auto (long "constants" <>
+               short 'c' <>
+               metavar "CONSTANTS" <>
+               showDefault <>
+               value 1 <>
+               help "Maximum number of constants to use")
+
+
+optFile :: Parser OptFile
+optFile = OptFile <$> strArgument (metavar "FILE")
+
+
+optsInfo :: ParserInfo Opts
+optsInfo = info (opts <**> helper)
+           (fullDesc <>
+            header "outsynth2 -- a PBE synthesizer for OutSystems expressions")
+
+
+lib :: [Component]
+lib = [ concat_
+      , length_
+      , replace_
+      , substr_
+      , tolower_
+      , toupper_
+      , trim_
+      , trim_start_
+      , trim_end_
+      , add_
+      , sub_]
+
+
+runSynth :: Opts -> IO ()
+runSynth opts = do
+      examples <- take (numExamples opts) <$>
+                  readExamplesFromFile (file opts)
+
+      putStrLn $ "=== Examples (" <> file opts <> ")"
+      mapM_ (putStrLn . showExample) examples
+
+      putStrLn "=== Library"
+      mapM_ (putStrLn . name) lib
+
+      putStrLn "=== Synthesis"
+      timeIt $ synthesize lib (numConsts opts) examples
+
+
+main :: IO ()
+main = runSynth =<< execParser optsInfo
